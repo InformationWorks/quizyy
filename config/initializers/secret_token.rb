d9911162d0ed300c5e0905b2c872a6e1d8275c20
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2012-11-11 16:27:10 +0530

    Executed "rails new gremasters-web --database postgresql"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GremastersWeb::Application.config.secret_token = '41b7b4b86a479a738e87520f1c7d8dc444f507585f38ab8554e3d36f3b0debdedd8ed730e2785f90a9cbd49fa09758f65704113b08dd26023a18058c1ed79ac3'
