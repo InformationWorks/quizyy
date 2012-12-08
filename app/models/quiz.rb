dddaa6c656d6f00960560504bafd5616d194f9d6
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2012-12-08 16:43:12 +0530

    Implemented dropdown list for all foreign keys. Task#2685043332318

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,5 +2,5 @@ class Quiz < ActiveRecord::Base
   belongs_to :quiz_type
   belongs_to :category
   belongs_to :topic
-  attr_accessible :name, :random
+  attr_accessible :name, :random, :quiz_type_id, :category_id, :topic_id
 end
