ba6afabcd35707ff587f379cd16a2d0027ca799d
Deep Shah <deep5187@gmail.com>
2012-12-22 21:23:24 +0530

    combined eveything in application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,15 @@
 //= require backbone/marionette/backbone.marionette
 //= require backbone/marionette/jquery.resolved
 //= require gre340
+//= require_tree ../templates
+//= require gre340.routing
+//= require ./testcenter/gre340.testcenter
+//= require ./testcenter/gre340.testcenter.routing
+//= require ./testcenter/gre340.testcenter.layout
+//= require ./testcenter/gre340.testcenter.models
+//= require ./testcenter/gre340.testcenter.views
+//= require ./testcenter/gre340.testcenter.controllers
+
 Backbone.Marionette.Renderer.render = function(template, data){
     if (!JST[template]) throw "Template '" + template + "' not found!";
     return JST[template](data);
--- app/assets/javascripts/gre340.testcenter.require.js.coffee
+++
@@ -1,9 +0,0 @@
-#= require_tree ../templates
-#= require gre340.routing
-#= require ./testcenter/gre340.testcenter
-#= require ./testcenter/gre340.testcenter.routing
-#= require ./testcenter/gre340.testcenter.layout
-#= require ./testcenter/gre340.testcenter.models
-#= require ./testcenter/gre340.testcenter.views
-#= require ./testcenter/gre340.testcenter.controllers
-
