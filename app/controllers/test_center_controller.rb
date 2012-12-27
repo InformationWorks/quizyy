e5ff994098aac3fac49e32e5a5337ca85bbcc5bf
Deep Shah <deep5187@gmail.com>
2012-12-27 21:24:27 +0530

    change if condition to unless

--- app/controllers/test_center_controller.rb
+++ app/controllers/test_center_controller.rb
@@ -7,9 +7,7 @@ class TestCenterController < ApplicationController
     if params[:quiz_id]
       #cookies[:current_quiz_id] = params[:quiz_id]
       @attempt = Attempt.where(["user_id = ? AND quiz_id = ?",current_user.id.to_s,"#{params[:quiz_id].to_i}"]).first_or_create(:user_id=> current_user.id,:quiz_id=>"#{params[:quiz_id].to_i}")
-      if (@attempt.is_current.nil? || @attempt.is_current==false)
-        @attempt.set_attempt_as_current
-      end
+      @attempt.set_attempt_as_current unless(@attempt.is_current)
     end
     end
 end
