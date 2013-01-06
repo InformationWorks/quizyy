ccf9feda1171278a2a308542cdd6793f8af9950d
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-01-07 03:21:14 +0530

    Integrate admin theme in categories/show /edit /new /index.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   attr_accessible :code, :name
   has_many :quizzes
+  
+  validates :code,:name, :presence => true
+  validates :code, :uniqueness => true
+  validates :name, :uniqueness => true
 end
