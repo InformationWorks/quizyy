57769eae8c4ed832f9b6ea40a9ff28fcb9da772a
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-01-12 00:11:16 +0530

    Fix merge conflict in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130109220635) do
+ActiveRecord::Schema.define(:version => 20130110172903) do
 
   create_table "attempt_details", :force => true do |t|
     t.integer  "attempt_id"
@@ -210,6 +210,7 @@ ActiveRecord::Schema.define(:version => 20130109220635) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "profile_image"
+    t.integer  "credits",                :default => 0
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
