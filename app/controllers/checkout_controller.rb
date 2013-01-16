f687d6b761e07426fb859e6848d5765f033c95de
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-01-17 05:21:11 +0530

    Process order based on the response from Zaakpay.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -41,7 +41,56 @@ class CheckoutController < ApplicationController
     zr = Zaakpay::Response.new(request.raw_post)  
     @checksum_check = zr.valid?
     @zaakpay_post = zr.all_params
-    #render :layout => false
+    
+    # Log Error if checksum does not match
+    # Process order for user's account only if the checksum matched.
+    if !@checksum_check
+      logger.info("CHECKSUM MISMATCH FROM IP-ADDRESS " + request.remote_ip.to_s)
+    else
+      # Save order values.
+      order = save_order(@zaakpay_post)
+      
+      # If responseCode == 100, add the quizzes/packages to user's account.
+      if order.responseCode == 100
+        process_order(order)
+      end
+    end
+  end
+  
+  private
+  
+  def save_order(params)
+    order = Order.find(params["orderId"].to_i)
+    order.responseCode = params["responseCode"].to_i
+    order.responseDescription = params["responseDescription"].to_s
+    order.save!
+    order
+  end
+  
+  def process_order(order)
+    cart = Cart.find(order.cart_id)
+        
+    # Add each cartitem to user's account.
+    cart.cart_items.each do | cart_item |
+      
+      if cart_item.quiz_id != nil
+      # Add Quiz
+        quiz_user = QuizUser.new
+        quiz_user.quiz_id = cart_item.quiz_id
+        quiz_user.user_id = current_user.id
+        quiz_user.save!
+        
+      elsif cart_item.package_id != nil
+      # Add Package
+        package_quizzes_ids = Package.find(cart_item.package_id).quizzes.pluck(:quiz_id)
+        package_quizzes_ids.each do |quiz_id|
+          quiz_user = QuizUser.new
+          quiz_user.quiz_id = quiz_id
+          quiz_user.user_id = current_user.id
+          quiz_user.save!
+        end  
+       end        
+    end
   end
   
 end
