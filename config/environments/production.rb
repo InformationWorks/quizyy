76c62e0b70631ff865f77dc58b717ac643ec4929
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-01-21 13:18:35 +0530

    Add default url options pointing to actual host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,10 @@ GremastersWeb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Heroku support : http://goo.gl/02SlZ
+  config.action_mailer.default_url_options = { :host => ENV['GRE340_HOST'] }
+  
 end
 
 ActionMailer::Base.delivery_method = :smtp
