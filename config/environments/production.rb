59f34f057f1b517cb3b8743a2267f513f176ec22
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-01-21 15:58:49 +0530

    Fix action_mailer smtp_settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,16 +68,15 @@ GremastersWeb::Application.configure do
   
   # Heroku support : http://goo.gl/02SlZ
   config.action_mailer.default_url_options = { :host => ENV['GRE340_HOST'] }
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => ENV['GMAIL_SMTP_USER'],
+    :password             => ENV['GMAIL_SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+   
 end
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-   :tls => true,
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :authentication => :login,
-   :domain => ENV['GMAIL_SMTP_USER'],
-   :user_name => ENV['GMAIL_SMTP_USER'],
-   :password => ENV['GMAIL_SMTP_PASSWORD']
-}
