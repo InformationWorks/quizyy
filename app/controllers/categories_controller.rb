b0cd0446d3df18e17e997702532c06886740c19f
Harshal <harshal.c.bhakta@gmail.com>
2013-01-23 00:04:45 +0530

    Add section_type fields to categories => _form/show/index.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.order(:section_type_id).all
 
     respond_to do |format|
       format.html # index.html.erb
