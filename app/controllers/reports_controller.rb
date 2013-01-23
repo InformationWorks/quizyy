51ff10858d3b2a6103096b6e254ae3f106d98aeb
Deep Shah <deep5187@gmail.com>
2013-01-24 02:35:29 +0530

    show score scaled to 340 in report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,13 +8,14 @@ class ReportsController < ApplicationController
     @attempt = Attempt.find_by_quiz_id_and_user_id(@quiz_id,current_user.id)
     @full_report = @attempt[:report].nil? ? @attempt[:report].each{|k,v| @attempt[:report][k] = eval(v)} : @attempt.calculate_score()[:report].each{|k,v| @attempt[:report][k] = eval(v)}
     section_types = SectionType.all()
-    main_section_report = Hash[section_types.map{|t| [t.name => {'correct' => 0,'total' =>0 }]}]
-    #@full_report['section_report'].each do |key,value|
-    #  main_section_report[value['section_type_name']]['correct'] =+ value['correct']
-    #end
+    main_section_report = Hash[section_types.map{|t| [t.name, Hash['correct' => 0,'total' =>0 ]]}]
+    @full_report['section_report'].each do |key,value|
+      main_section_report[value['section_type']]['correct'] += value['correct']
+      main_section_report[value['section_type']]['total'] += value['total']
+    end
     @full_report['main_section_report'] = main_section_report
     highest_score_report = @attempt.get_with_highest_score()[:report] || @attempt.get_with_highest_score().calculate_score()[:report]
-    @full_report[:highest_attempt] = highest_score_report.each{|k,v| highest_score_report[k] = eval(v)}
+    @full_report['highest_attempt'] = highest_score_report.each{|k,v| highest_score_report[k] = eval(v)}
     respond_to do |format|
       format.html # show.html.erb
     end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,8 @@
 module ReportsHelper
+  def total_scaled_score(score_in_percent)
+    (340 * score_in_percent)/100
+  end
+  def section_scaled_score(score_in_percent)
+    (170 * score_in_percent)/100
+  end
 end
