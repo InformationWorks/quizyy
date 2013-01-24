64a0795951a9c047cc0cf20f5c96baa040ab8a2c
Harshal <harshal.c.bhakta@gmail.com>
2013-01-24 17:58:39 +0530

    Use slug to refer to a package.

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,7 +1,8 @@
 class PackagesController < ApplicationController
   
   before_filter :authenticate_user!
-  load_and_authorize_resource
+  before_filter :load_package, :only => [ :show, :edit, :update, :destroy, :destroy_quiz_from_package, :add_quiz_to_package ]
+  load_and_authorize_resource :find_by => :slug
   
   # GET /packages
   # GET /packages.json
@@ -17,7 +18,6 @@ class PackagesController < ApplicationController
   # GET /packages/1
   # GET /packages/1.json
   def show
-    @package = Package.find(params[:id])
     
     # NOTE: "NOT IN" does not work when it gets an empty array.
     if @package.quizzes.count > 0
@@ -45,7 +45,6 @@ class PackagesController < ApplicationController
 
   # GET /packages/1/edit
   def edit
-    @package = Package.find(params[:id])
   end
 
   # POST /packages
@@ -67,8 +66,6 @@ class PackagesController < ApplicationController
   # PUT /packages/1
   # PUT /packages/1.json
   def update
-    @package = Package.find(params[:id])
-
     respond_to do |format|
       if @package.update_attributes(params[:package])
         format.html { redirect_to @package, notice: 'Package was successfully updated.' }
@@ -83,7 +80,7 @@ class PackagesController < ApplicationController
   # DELETE /packages/1
   # DELETE /packages/1.json
   def destroy
-    @package = Package.find(params[:id])
+    
     @package.destroy
 
     respond_to do |format|
@@ -95,38 +92,36 @@ class PackagesController < ApplicationController
   # /packages/:id/destroy_quiz_from_package/:quiz_id
   # DELETE /packages/:id/destroy_quiz_from_package/:quiz_id.json
   def destroy_quiz_from_package
-    
-    @package = Package.find(params[:id])
-    
     begin
-      PackageQuiz.delete_all(["package_id = ? AND quiz_id = ?",params[:id],params[:quiz_id]])
+      PackageQuiz.delete_all(["package_id = ? AND quiz_id = ?",@package.id,params[:quiz_id]])
       redirect_to @package, notice: 'Quiz was successfully removed from the package.'
     rescue Exception => ex
       logger.info("Error 1001: Error removing quiz from package : " + ex.message)
       redirect_to @package, notice: 'Error 1001: Error removing quiz from the package. '
     end
-    
   end
   
   # /packages/:id/add_quiz_to_package/:quiz_id
   # POST /packages/:id/add_quiz_to_package/:quiz_id.json
   def add_quiz_to_package
-    
-    @package = Package.find(params[:id])
-    
     if params[:quiz][:id] == "" || params[:quiz][:id] == nil
       redirect_to @package, notice: 'Please select a quiz to add.'
     else
       package_quiz = PackageQuiz.new
       package_quiz.quiz_id = params[:quiz][:id]
-      package_quiz.package_id = params[:id]
+      package_quiz.package_id = @package.id
       if package_quiz.save
         redirect_to @package, notice: 'Quiz added successfully to the package.'
       else
         redirect_to @package, notice: 'Error adding quiz to the package.'
       end
     end
-    
+  end
+  
+  private
+  
+  def load_package
+    @package = Package.find_by_slug!(params[:id])
   end
   
 end
