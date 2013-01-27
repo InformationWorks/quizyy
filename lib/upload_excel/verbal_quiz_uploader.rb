11ff47a9cd87cb219c73bdebbd62b683615329a7
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-01-27 13:32:11 +0530

    Verbal quiz upload works.

--- lib/upload_excel/verbal_quiz_uploader.rb
+++ lib/upload_excel/verbal_quiz_uploader.rb
@@ -15,9 +15,9 @@ module UploadExcel
       
       # Hold the section objects that need to be saved to db.
       # Question & Option objects will be saved under the section object.
-      @sections = []
-      @questions = [[]]
-      @options = [[[]]]
+      @verbal_section = nil
+      @questions = []
+      @options = [[]]
       
       @verbal_sheet = @sheets[0]
       @curr_question_index = 0
@@ -75,7 +75,22 @@ module UploadExcel
     # return true is everything goes well.
     # return false if there us any error.
     def execute_excel_upload
-      @success_messages << "Quiz uploaded successfully."
+      
+      clear_quiz
+      
+      # return false if processing fails for a sheet.
+      if !process_verbal_sheet
+        return false
+      end
+      
+      # Save the objects stored in the array.
+      if !save_objects_to_db
+        @error_messages << "Saving objects to db failed"
+        return false
+      end
+      
+      @success_messages << ("1 section uploaded.")
+      @success_messages << ("section 1 #{@questions.count.to_s} Questions uploaded")
       
       return true
     end
@@ -164,6 +179,309 @@ module UploadExcel
       # TODO: Return false if a invalid entry found.
       
       return true
+    end
+    
+    # Process a verbal sheet.
+    # return true is processing is successful.
+    # return false for unexpected results.
+    def process_verbal_sheet
+    
+      # Create the verbal section
+      @verbal_section = Section.new :name => @verbal_sheet.name, 
+                                   :sequence_no => 1, 
+                                   :section_type_id => SectionType.find_by_name("Verbal").id
+      @verbal_section.quiz = @quiz
+      
+      # Create questions under the created section
+      create_verbal_questions(@verbal_section)
+      
+      Rails.logger.info("Section added. Section count = " + @verbal_section.to_s)
+        
+      return true
+    end
+    
+    # Create question objects by reading each row of the current sheet.
+    # Add questions to the section object.
+    def create_verbal_questions(section)
+      
+      # Start from row at index 1
+      (1..20).each do |row_index|
+        
+        # Get current question and add it to the section.
+        curr_que = getVerbalQuestionFromRow(row_index)
+        
+        @questions << curr_que
+        
+      end
+        
+    end
+    
+    # Generate a question object by reading a row from excel.
+    # Also add option objects to the question object.
+    def getVerbalQuestionFromRow(row_index)
+      
+      Rails.logger.info("Creating verbal question = " + row_index.to_s)
+      
+      row = @verbal_sheet.row(row_index)
+      
+      question = Question.new
+      
+      begin
+        
+        sequence_no = row[0].to_s.strip
+        question.sequence_no = (sequence_no == "") ? nil : sequence_no.to_i
+        
+        instruction = row[2].to_s.strip
+        question.instruction = (instruction == "") ? nil : instruction
+        
+        passage = row[3].to_s.strip
+        question.passage = (passage == "") ? nil : passage
+        
+        que_text = row[4].to_s.strip
+        question.que_text = (que_text == "") ? nil : que_text
+        
+        sol_text = row[5].to_s.strip
+        question.sol_text = (sol_text == "") ? nil : sol_text
+        
+        option_set_count = row[6].to_s.strip
+        question.option_set_count = (option_set_count == "") ? nil : row[6].to_i        
+
+        question.que_image = nil
+        question.sol_image = nil
+        question.di_location = nil
+        question.quantity_a = nil
+        question.quantity_b = nil
+        
+        # Question references.
+        question.type = Type.find_by_code(row[1].to_s)
+        question.topic = nil
+      
+        Rails.logger.info("curr_sheet_index = " + @curr_sheet_index.to_s + " row_index = " + row_index.to_s)
+        
+        @options[row_index-1] = []
+        
+        build_options_for_question(question,row,row_index)
+        
+      rescue Exception => e
+        Rails.logger.info("ERROR_EXCEL_getQuestionFromRow:" + e.message.to_s)
+        return nil
+      end
+        
+      
+      return question
+      
+      
+    end
+    
+     # Generate a question object by reading a row from excel.
+    # Also add option objects to the question object.
+    def build_options_for_question(question,row,row_index)
+      
+      options = []
+      
+      if question.type == Type.find_by_code("V-MCQ-1")
+        
+        option = row[8].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 1) 
+        
+        option = row[9].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 2) 
+        
+        option = row[10].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 3)
+        
+        option = row[11].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 4)
+        
+        option = row[12].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 5)
+        
+        # set correct option 
+        options[row[7].to_i-1].correct = true
+        
+      elsif question.type == Type.find_by_code("V-MCQ-2")
+        
+        option = row[8].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 1)
+        
+        option = row[9].to_s.strip 
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 2)
+         
+        option = row[10].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 3)
+        
+        # set correct options
+        row[7].to_s.split(",").each do |correct_index|
+           options[correct_index.to_i-1].correct = true
+        end
+        
+      elsif question.type == Type.find_by_code("V-SIP")
+        # Option with 0 based answer sentence index.
+        option = row[7].to_s.strip
+        option_int = option.to_i - 1
+        options << (Option.new :content => (option == "" ? nil : option_int),:correct => true,:sequence_no => 1)
+        
+      elsif question.type == Type.find_by_code("V-TC-1")
+        
+        option = row[8].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 1)
+        
+        option = row[9].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 2)
+        
+        option = row[10].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 3)
+        
+        option = row[11].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 4)
+        
+        option = row[12].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 5)
+        
+        # set correct option 
+        options[row[7].to_i-1].correct = true
+        
+      elsif question.type == Type.find_by_code("V-TC-2")
+        
+        option = row[8].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 1)
+        
+        option = row[9].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 2)
+        
+        option = row[10].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 3)
+        
+        option = row[11].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 4)
+        
+        option = row[12].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 5)
+        
+        option = row[13].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 6)
+
+        # set correct options
+        correct_options = row[7].to_s.split(",")
+        
+        options[correct_options[0].to_i-1].correct = true
+        options[correct_options[1].to_i+2].correct = true
+        
+      elsif question.type == Type.find_by_code("V-TC-3")
+        
+        option = row[8].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 1)
+        
+        option = row[9].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 2)
+        
+        option = row[10].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 3)
+        
+        option = row[11].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 4)
+        
+        option = row[12].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 5)
+        
+        option = row[13].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 6)
+        
+        option = row[14].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 7)
+        
+        option = row[15].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 8)
+        
+        option = row[16].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 9)
+        
+        # set correct options
+        correct_options = row[7].to_s.split(",")
+        
+        options[correct_options[0].to_i-1].correct = true
+        options[correct_options[1].to_i+2].correct = true
+        options[correct_options[2].to_i+5].correct = true
+        
+      elsif question.type == Type.find_by_code("V-SE")
+        
+        option = row[8].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 1)
+        
+        option = row[9].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 2)
+        
+        option = row[10].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 3)
+        
+        option = row[11].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 4)
+        
+        option = row[12].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 5)
+        
+        option = row[13].to_s.strip
+        options << (Option.new :content => (option == "" ? nil : option),:correct => false,:sequence_no => 6)
+        
+        # set correct options
+        correct_options = row[7].to_s.split(",")
+        
+        options[correct_options[0].to_i-1].correct = true
+        options[correct_options[1].to_i-1].correct = true
+      
+      end
+      
+      @options[row_index-1] = options
+      
+    end
+    
+    # Save all the objects to db.
+    # Roll back if any of the object fails to save.
+    def save_objects_to_db
+      
+      if !@verbal_section.save
+        roll_back_created_objects
+        return false
+      end
+      
+      @questions.each_with_index do |question,question_index|
+        
+        question.section_id = @verbal_section.id
+        
+        if !question.save
+          roll_back_created_objects
+          return false
+        end
+      
+        @options[question_index].each_with_index do |option,option_index|
+          
+          option.question_id = question.id
+          
+          if !option.save
+            roll_back_created_objects(section_index)
+            return false
+          end
+            
+        end
+        
+      end
+      
+      return true
+    end
+    
+    # Roll-back the entire section
+    def roll_back_created_objects
+        section.destroy
+    end
+    
+    # Delete all the sections, questions & options before uploading.
+    def clear_quiz
+      
+      # Section -> Question -> Option [ Dependent destroy ] 
+      @quiz.sections.each do |section|
+        section.destroy
+      end
+      
     end
     
   end
