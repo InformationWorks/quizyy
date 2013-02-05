c58ddc4a2c92df5ae5d17558a46858f488287160
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-02-05 18:42:54 +0530

    Add a way to make quiz timed/untimed.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,7 @@ class Quiz < ActiveRecord::Base
   belongs_to :quiz_type
   belongs_to :category
   belongs_to :topic
-  attr_accessible :name, :random, :quiz_type_id, :category_id, :topic_id,:desc
+  attr_accessible :name, :timed, :random, :quiz_type_id, :category_id, :topic_id,:desc
   attr_accessor :word
   validates :name,:desc,:slug,:price, :presence => true
   before_validation :generate_slug
