33388ed994e9d4fe7eff1ab29ddac9e1999832fd
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-04-05 13:39:11 +0530

    Modified deployment config files for ashram quizyy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:iwadmin/#{application}.git"
-set :branch, "master"
+set :branch, "ashram"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
@@ -41,8 +41,8 @@ namespace :deploy do
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
+    unless `git rev-parse HEAD` == `git rev-parse origin/ashram`
+      puts "WARNING: HEAD is not the same as origin/ashram"
       puts "Run `git push` to sync changes."
       exit
     end
