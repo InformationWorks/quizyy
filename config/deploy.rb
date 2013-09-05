51adb9a681c5ecc6df32be91e78dc3becf91f99b
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-09-05 18:32:56 +0530

    Add symlink for application.yml - figaro

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
